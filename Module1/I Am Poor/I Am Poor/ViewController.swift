9a2b73c3d3f5811b1d04d14d410dc7b38dbd4851
Len <48297915+Dev2L@users.noreply.github.com>
2020-06-03 00:22:08 -0400

    Add files via upload

---
+++ Module1/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by El$ on 6/2/20.
+//  Copyright © 2020 Len. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
